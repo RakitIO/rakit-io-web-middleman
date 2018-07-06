66734d243e5623fc234443e82f4fa8df4ea3bb13
Prasetya Mahardhitama <bluekurv@gmail.com>
2018-07-06 13:18:14 +0700

    Added minification during build.

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+activate :asset_hash
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -40,7 +42,8 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :minify_html
+end
